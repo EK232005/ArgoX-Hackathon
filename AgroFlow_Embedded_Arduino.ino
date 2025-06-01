be03e441c419dc5404b0be656d6face05591b9c8
Eshaan Matthew Kuttikad <85567814+EK232005@users.noreply.github.com>
2025-06-01 10:56:42 +0530

    Add files via upload

--- AgroFlow_Embedded_Arduino.ino
+++ AgroFlow_Embedded_Arduino.ino
@@ -19,6 +19,30 @@ float turbval[10], turbtemp, avgturbvoltage, turbvalout;
 
 int modevar = 1;
 
+//Dosing and Flushing 
+#define FLUSH_PIN 3     // Example motor driver pin for flush
+#define DOSE1_PIN 4     // Fertilizer 1 PWM pin
+#define DOSE2_PIN 5     // Fertilizer 2 PWM pin
+
+void flushSystem() {
+  analogWrite(FLUSH_PIN, 180);
+  delay(3000); // 3 seconds flushing
+  analogWrite(FLUSH_PIN, 0);
+}
+
+void doseFertilizer1() {
+  analogWrite(DOSE1_PIN, 150);
+  delay(2000); // 2 seconds dosing
+  analogWrite(DOSE1_PIN, 0);
+}
+
+void doseFertilizer2() {
+  analogWrite(DOSE2_PIN, 150);
+  delay(2000); // 2 seconds dosing
+  analogWrite(DOSE2_PIN, 0);
+}
+
+
 void setup() {
   Serial.begin(9600);
   //pH setup
@@ -32,6 +56,20 @@ void setup() {
   pinMode(turbsensorpin, INPUT);  //Set the turbidity sensor pin to input mode
 }
 void loop() {
+  // Command reception from Raspberry Pi
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();  // Remove newline/extra whitespace
+
+    if (cmd == "FLUSH") {
+      flushSystem();
+    } else if (cmd == "DOSE1") {
+      doseFertilizer1();
+    } else if (cmd == "DOSE2") {
+      doseFertilizer2();
+    }
+  }
+
   switch (modevar) {
     case 1:
       gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
@@ -56,9 +94,9 @@ void loop() {
         }
       }
       avgValue = 0;
-      for (int i = 2; i < 8; i++) avgValue += buf[i];    //take the average value of 6 center sample
-      phValue = (float)avgValue * 5.0 / 1024 / 6;  //convert the analog into millivolt
-      phValue = -5.70 * phValue + calibration_value;     //convert the millivolt into pH value
+      for (int i = 2; i < 8; i++) avgValue += buf[i];  //take the average value of 6 center sample
+      phValue = (float)avgValue * 5.0 / 1024 / 6;      //convert the analog into millivolt
+      phValue = -5.70 * phValue + calibration_value;   //convert the millivolt into pH value
       modevar++;
       break;
 
@@ -85,14 +123,14 @@ void loop() {
       turbvalout = constrain(turbvalout, 0, 3000);
       modevar++;
       break;
-    
+
     default:
       modevar = 1;
       float ecValue = tdsValue / 640.0;
-      Serial.print("TDS: "); Serial.print(tdsValue, 0); Serial.println("ppm");
-      Serial.print("EC: "); Serial.print(ecValue, 2); Serial.println(" mS/cm");
-      Serial.print("pH: "); Serial.println(phValue);
-      Serial.print("Turbidity: "); Serial.println(turbvalout);
+      Serial.print("TDS: ");Serial.print(tdsValue, 0);Serial.println("ppm");
+      Serial.print("EC: ");Serial.print(ecValue, 2);Serial.println(" mS/cm");
+      Serial.print("pH: ");Serial.println(phValue);
+      Serial.print("Turbidity: ");Serial.println(turbvalout);Serial.println("NTU");
       Serial.println(" - - - - - - - - - - - - - - - - ");
       delay(1000);
       break;
